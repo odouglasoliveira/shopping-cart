c8fb87710d86a4f80e8fdf270a07c852fae1c861
Douglas Oliveira <doglasolivera13@gmail.com>
2023-02-14 16:32:20 -0300

    Requisitos 1 e 2 concluídos

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,12 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (item) => {
+  try {
+    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${item}`);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    throw new Error('Termo de busca não informado');
+  }
 };
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -3,19 +3,29 @@ import { fetchProductsList } from '../helpers/fetchFunctions';
 import computadorSearch from './mocks/search';
 
 // implemente seus testes aqui
-describe('Teste a função fetchProductsList', () => {
+describe('Teste a função fetchProductsList',  () => {
   it('fetchProductsList é uma função', () => {
     expect(typeof fetchProductsList).toBe('function');
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalled()
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   });
 
-  // it('...', () => {
-  // });
+  it('testa se a função retorna o objeto que deve retornar', async () => {
+    expect(await fetchProductsList('computador')).toEqual(computadorSearch);
+  });
+
+  it('testa se retorna um erro quando a função é chamada sem parametro', async () => {
+    try {
+      await fetchProductsList();
+    } catch (error) {
+      expect(error.message).toMatch('Termo de busca não informado');  
+    }});
 });
