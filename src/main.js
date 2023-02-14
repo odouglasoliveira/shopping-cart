1e7436ac28e324da763148e258b6204d85c77990
Douglas Oliveira <doglasolivera13@gmail.com>
2023-02-14 19:14:04 -0300

    Requisito 4 concluido

--- src/main.js
+++ src/main.js
@@ -3,12 +3,25 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
+const display = document.querySelector('.products');
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
+const showLoading = () => {
+  const loadingElement = document.createElement('span');
+  loadingElement.innerHTML = 'carregando...';
+  loadingElement.classList.add('loading');
+  display.appendChild(loadingElement);
+};
+showLoading();
+
+const cleanLoading = () => {
+  display.innerHTML = '';
+};
+
 const listProducts = async () => {
-  const display = document.querySelector('.products');
   const products = await fetchProductsList('computador');
   console.log(products);
+  cleanLoading();
   products.forEach((product) => {
     display.appendChild(createProductElement(product));
   });
