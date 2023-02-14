66a88475e257cb9be3c31936b03af4e9a4dffc29
Douglas Oliveira <doglasolivera13@gmail.com>
2023-02-14 17:01:59 -0300

    Refatoração do requisito 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -4,11 +4,7 @@ export const fetchProduct = () => {
 
 export const fetchProductsList = async (item) => {
   if (!item) throw new Error('Termo de busca não informado');
-  try {
-    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${item}`);
-    const data = await response.json();
-    return data.results;
-  } catch (error) {
-    return new Error('Termo de busca não informado');
-  }
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${item}`);
+  const data = await response.json();
+  return data.results;
 };
