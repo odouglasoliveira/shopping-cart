83317c96de8eaef1daac806cd61d9341c8399b1d
Douglas Oliveira <doglasolivera13@gmail.com>
2023-02-14 21:16:59 -0300

    Requisito 6 concluido

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,21 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('Testa se a fetchProduct é uma função', () => {
+    expect(typeof fetchProduct).toBe('function');
   });
+  it('Testa se o fetch é chamado', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('Testa se a função utiliza o endpoint correto', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toBeCalledWith('https://api.mercadolibre.com/items/MLB1405519561');
+  });
+  it('Testa se o retorno da função é correto', async () => {
+    expect(fetchProduct('MLB1405519561')).toBe(product);
+  });
+  it('Testa se ao chamar a função sem argumento, retorna um erro', async () => {
+    await expect(fetchProduct()).rejects.toThrow('ID não informado');
+  })
 });
