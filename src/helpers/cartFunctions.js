45eafc015b415ac9efc75ae84bb90635decc87b0
Douglas Oliveira <doglasolivera13@gmail.com>
2023-07-20 02:27:38 -0300

    correção do subtotal

--- src/helpers/cartFunctions.js
+++ src/helpers/cartFunctions.js
@@ -1,19 +1,11 @@
 import updatePrice from './updateCost';
 import { fetchProduct } from './fetchFunctions';
 
-/**
- * Função que retorna todos os itens do carrinho salvos no localStorage.
- * @returns {Array} Itens de ids salvos do carrinho ou array vazio.
- */
 export const getSavedCartIDs = () => {
   const cartProducts = localStorage.getItem('cartProducts');
   return cartProducts ? JSON.parse(cartProducts) : [];
 };
 
-/**
- * Função que adiciona um product ao carrinho.
- * @param {string} id - ID do product a ser adicionado.
- */
 export const saveCartID = (id) => {
   if (!id) throw new Error('Você deve fornecer um ID');
 
@@ -22,17 +14,13 @@ export const saveCartID = (id) => {
   localStorage.setItem('cartProducts', JSON.stringify(newCartProducts));
 };
 
-/**
- * Função que remove um product do carrinho.
- * @param {string} id - ID do product a ser removido.
-*/
 export const subtractPrice = async (id) => {
   const totalPrice = localStorage.getItem('totalPrice');
   let totalNumber = +totalPrice;
   const product = await fetchProduct(id);
   const { price } = product;
   totalNumber -= price;
-  localStorage.setItem('totalPrice', totalNumber);
+  localStorage.setItem('totalPrice', totalNumber.toFixed(2));
   updatePrice();
   return totalNumber;
 };
--- src/helpers/shopFunctions.js
+++ src/helpers/shopFunctions.js
@@ -2,16 +2,6 @@ import { removeCartID, saveCartID } from './cartFunctions';
 import { fetchProduct } from './fetchFunctions';
 import updatePrice from './updateCost';
 
-// Esses comentários que estão antes de cada uma das funções são chamados de JSdoc,
-// experimente passar o mouse sobre o nome das funções e verá que elas possuem descrições!
-
-// Fique a vontade para modificar o código já escrito e criar suas próprias funções!
-
-/**
- * Função responsável por criar e retornar o elemento de imagem do produto.
- * @param {string} imageSource - URL da imagem.
- * @returns {Element} Elemento de imagem do produto.
- */
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'product__image';
@@ -19,13 +9,6 @@ const createProductImageElement = (imageSource) => {
   return img;
 };
 
-/**
- * Função responsável por criar e retornar qualquer elemento.
- * @param {string} element - Nome do elemento a ser criado.
- * @param {string} className - Classe do elemento.
- * @param {string} innerText - Texto do elemento.
- * @returns {Element} Elemento criado.
- */
 export const createCustomElement = (element, className, innerText = '') => {
   const e = document.createElement(element);
   e.className = className;
@@ -33,34 +16,15 @@ export const createCustomElement = (element, className, innerText = '') => {
   return e;
 };
 
-/**
- * Função que recupera o ID do produto passado como parâmetro.
- * @param {Element} product - Elemento do produto.
- * @returns {string} ID do produto.
- */
 export const getIdFromProduct = (product) => (
   product.querySelector('span.product__id').innerText
 );
 
-/**
- * Função que remove o produto do carrinho.
- * @param {Element} li - Elemento do produto a ser removido do carrinho.
- * @param {string} id - ID do produto a ser removido do carrinho.
- */
 const removeCartProduct = (li, id) => {
   li.remove();
   removeCartID(id);
 };
 
-/**
- * Função responsável por criar e retornar um product do carrinho.
- * @param {Object} product - Objeto do produto.
- * @param {string} product.id - ID do produto.
- * @param {string} product.title - Título do produto.
- * @param {string} product.price - Preço do produto.
- * @param {string} product.pictures - Imagens do produto.
- * @returns {Element} Elemento de um product do carrinho.
- */
 export const createCartProductElement = ({ id, title, price, pictures }) => {
   const li = document.createElement('li');
   li.className = 'cart__product';
@@ -93,22 +57,12 @@ export const createCartProductElement = ({ id, title, price, pictures }) => {
   return li;
 };
 
-/**
- * Função responsável por criar e retornar o elemento do produto.
- * @param {Object} product - Objeto do produto.
- * @param {string} product.id - ID do produto.
- * @param {string} product.title - Título do produto.
- * @param {string} product.thumbnail - URL da imagem do produto.
- * @param {number} product.price - Preço do produto.
- * @returns {Element} Elemento de produto.
-*/
-
 export const getProductPrice = async (id) => {
   const totalPrice = localStorage.getItem('totalPrice') || 0;
   let number = +totalPrice;
   const product = await fetchProduct(id);
   number += product.price;
-  localStorage.setItem('totalPrice', number);
+  localStorage.setItem('totalPrice', number.toFixed(2));
   updatePrice();
   return number;
 };
@@ -137,7 +91,6 @@ export const createProductElement = ({ id, title, thumbnail, price }) => {
   cartButton.addEventListener('click', async () => {
     saveCartID(id);
     getProductPrice(id);
-    console.log(await getProductPrice(id));
     const productContainer = document.querySelector('.cart__products');
     const product = createCartProductElement(await fetchProduct(id));
     productContainer.appendChild(product);
